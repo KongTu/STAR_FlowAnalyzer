2ea0cd9cca1f9467cc5a67f47ccaf06047deb520
Kong Tu <kong@zhoudunming-lt.star.bnl.gov>
2018-08-10 16:47:30 -0400

    add centrality dimention

--- StRoot/StFlowTreeMaker/StFlowTreeMaker.h
+++ StRoot/StFlowTreeMaker/StFlowTreeMaker.h
@@ -69,6 +69,7 @@ class StFlowTreeMaker : public StMaker {
 		Int_t    Make();
 		Int_t    Finish();
 
+		void     setVnHarmonics(const Double_t n_);
 		void     setMaxVtxR(const Double_t max);
 		void     setMaxVtxZ(const Double_t max);
 		void     setMaxVzDiff(const Double_t max);
